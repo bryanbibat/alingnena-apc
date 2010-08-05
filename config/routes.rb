dfc5107cec3fafd46d4961f55bcb3b40a71c0c76
Bry <bry@bryanbibat.com>
2010-08-05 14:38:23 +0800

    use named routes for show and index (p53-54)

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
   map.resources :debts
 
-  map.connect 'products/', :controller => 'products', :action => 'index'
-  map.connect 'products/:id', :controller => 'products', :action => 'show'
+  map.products 'products/', :controller => 'products', :action => 'index'
+  map.product 'products/:id', :controller => 'products', :action => 'show'
 end
