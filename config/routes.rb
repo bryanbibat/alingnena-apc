5159965a289d297d6460d026680d1ca282b91fa9
Bry <bry@bryanbibat.com>
2010-08-05 14:23:25 +0800

    show all products preparation (p46)

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
   map.resources :debts
 
-  map.connect('products/:id', { :controller => 'products', :action => 'show' })
+  map.connect 'products/', :controller => 'products', :action => 'index'
+  map.connect 'products/:id', :controller => 'products', :action => 'show'
 end
