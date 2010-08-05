2eff4fc4bda67c305185d1e9cb23153d3b807e15
Bry <bry@bryanbibat.com>
2010-08-05 17:56:21 +0800

    include search in RESTful routes (p76)

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
   map.resources :debts
 
-  map.search_products 'products/search', :controller => 'products', :action => 'search'
-  map.resources(:products)
+  map.resources(:products, { :collection => { :search => :get }})
 
 end
