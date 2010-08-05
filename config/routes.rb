ca1aa899fbced4bdc68aa595e21212db65d3ab8b
Bry <bry@bryanbibat.com>
2010-08-05 17:55:25 +0800

    convert to RESTful routes (p76)

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,7 @@
 ActionController::Routing::Routes.draw do |map|
   map.resources :debts
 
-  map.products 'products/', :controller => 'products', :action => 'index', :conditions => { :method => :get }
-  map.connect 'products/', :controller => 'products', :action => 'create',
-:conditions => { :method => :post } 
-  map.new_product 'products/new', :controller => 'products', :action => 'new'
   map.search_products 'products/search', :controller => 'products', :action => 'search'
-  map.connect 'products/:id', :controller => 'products', :action => 'destroy', :conditions => { :method => :delete }
-  map.product 'products/:id', :controller => 'products', :action => 'show', :conditions => { :method => :get }
-  map.connect 'products/:id', :controller => 'products', :action => 'update', :conditions => { :method => :put }
-  map.edit_product 'products/:id/edit', :controller => 'products', :action => 'edit'
+  map.resources(:products)
 
 end
