77a91e6b96e5bb21a8378544786bc8350f25dba1
Bry <bry@bryanbibat.com>
2010-08-07 07:28:55 +0800

    add session saving for locale (p153)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,8 +10,9 @@ class ApplicationController < ActionController::Base
   before_filter :set_locale
 
   private 
-    def set_locale
-      I18n.locale = params[:locale] 
+    def set_locale 
+      I18n.locale = params[:locale] || session[:locale] 
+      session[:locale] = params[:locale] if params.has_key? :locale
     end
   
 end
