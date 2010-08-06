392d1031a55f2200e34e2550030832ca01bd3360
Bry <bry@bryanbibat.com>
2010-08-07 01:30:13 +0800

    has and belongs to many example (p107)

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100806152443) do
+ActiveRecord::Schema.define(:version => 20100806172521) do
 
   create_table "debts", :force => true do |t|
     t.string   "name"
@@ -34,6 +34,11 @@ ActiveRecord::Schema.define(:version => 20100806152443) do
     t.integer  "stock"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "products_purchases", :id => false, :force => true do |t|
+    t.integer "product_id"
+    t.integer "purchase_id"
   end
 
   create_table "purchases", :force => true do |t|
