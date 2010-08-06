fcdedc2c0b04565d3473042d0bbc9981b6fcf1a8
Bry <bry@bryanbibat.com>
2010-08-07 03:13:39 +0800

    render :update example (p141)

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -11,10 +11,25 @@ class MessagesController < ApplicationController
   
   def create
     @message = Message.new(params[:message])
-    if @message.save
-      redirect_to messages_url, :notice => "Message was successfully created"
-    else
-      render :action => "index"
+    respond_to do |format|
+      if @message.save
+        format.html { redirect_to messages_url, :notice => "Message was successfully created" }
+        format.js do 
+          render :update do |page|
+            page.replace_html :message_list, 
+              :partial => Message.all(:order => "created_at desc")
+            page.replace_html :notice, 'Message was successfully created.'
+            page[:new_message].reset 
+          end
+        end
+      else
+        format.html { render :action => "index" }
+        format.js do 
+          render :update do |page| 
+            page.replace_html :notice, 'There was an error in creating the message.'
+          end 
+        end
+      end
     end
   end
 
