fc9918ac5a4a958694992a6b107b9a3cbf8cf2ec
Bry <bry@bryanbibat.com>
2010-08-07 03:17:08 +0800

    move code to RJS template (p144)

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -14,14 +14,7 @@ class MessagesController < ApplicationController
     respond_to do |format|
       if @message.save
         format.html { redirect_to messages_url, :notice => "Message was successfully created" }
-        format.js do 
-          render :update do |page|
-            page.replace_html :message_list, 
-              :partial => Message.all(:order => "created_at desc")
-            page.replace_html :notice, 'Message was successfully created.'
-            page[:new_message].reset 
-          end
-        end
+        format.js
       else
         format.html { render :action => "index" }
         format.js do 
