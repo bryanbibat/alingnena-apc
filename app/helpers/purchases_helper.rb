d82cba15312df0db2c34adea855b7e3ad645892d
Bry <bry@bryanbibat.com>
2010-08-06 22:50:59 +0800

    replace concat with a better approach (p102)

--- app/helpers/purchases_helper.rb
+++ app/helpers/purchases_helper.rb
@@ -1,18 +1,9 @@
 module PurchasesHelper
   def display_invoice(purchase) 
     unless purchase.invoice.nil?
-      concat("<p><b>Invoice Reference Number:</b>\n") 
-      concat(link_to h(@purchase.invoice.reference_number),
-             edit_purchase_invoice_url(@purchase)) 
-      concat("</p>")
-      concat("<p>")
-      concat(link_to "Delete Invoice", purchase_invoice_url(@purchase),
-             :confirm => "Are you sure?", :method => :delete)
-      concat("</p>")
-    else 
-      concat("<p>") 
-      concat(link_to "Create Invoice", new_purchase_invoice_url(@purchase)) 
-      concat("</p>")
-    end 
+      render :partial => "invoice", :locals => { :purchase => @purchase } 
+    else
+      render :partial => "no_invoice", :locals => { :purchase => @purchase } 
+    end
   end
 end
